b4f31ba770ac3c2f76ab4816cc9c8da961370c48
Turito Yuenan <trietntm@hotmail.com>
2021-12-16 13:12:20 +0700

    reset

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-# Fabric Example Mod
-
-## Setup
-
-For setup instructions please see the [fabric wiki page](https://fabricmc.net/wiki/tutorial:setup) that relates to the IDE that you are using.
-
-## License
-
-This template is available under the CC0 license. Feel free to learn from it and incorporate it in your own projects.
+![banner](banner.png)
+# Minefetch - the Minecraft mod
+Add a fetch command in Minecraft that gives report about version, position, game time, number of mods...
---
+++ src/main/java/vn/turito/minefetch/Minefetch.java
@@ -0,0 +1,40 @@
+package vn.turito.minefetch;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.server.command.*;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.*;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+
+
+public class Minefetch implements ModInitializer {
+	private int Fetch(CommandContext<ServerCommandSource> ctx) throws CommandSyntaxException {
+		ServerCommandSource sauce = ctx.getSource();
+		ServerWorld world         = sauce.getWorld();
+		ServerPlayerEntity player = sauce.getPlayer();
+
+		String positions = player.getBlockX()+", "+player.getBlockY()+", "+player.getBlockZ();
+
+		Text[] fetches = {
+			new LiteralText(String.valueOf("██████████  "+"@"+player.getDisplayName().asString())),
+			new LiteralText(String.valueOf("██░░██░░██  "+"Seed: "+world.getSeed())),
+			new LiteralText(String.valueOf("████░░████  "+"Time: "+world.getTime())),
+			new LiteralText(String.valueOf("███░░░░███  "+"Position: "+positions)),
+			new LiteralText(String.valueOf("███░██░███  ")),
+			new LiteralText(String.valueOf("██████████  ")),
+		};
+
+		for (Text i: fetches) {sauce.sendFeedback(i, false);}
+		return 1;
+	}
+
+	@Override
+	public void onInitialize() {
+		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) ->
+			dispatcher.register(CommandManager.literal("minefetch").executes(ctx -> this.Fetch(ctx)))
+		);
+	}
+}
