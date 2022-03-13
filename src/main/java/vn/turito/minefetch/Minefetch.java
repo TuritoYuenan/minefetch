55c71acae223ca4b852aa2d29de19af6456364e3
Turito Yuenan <trietntm@hotmail.com>
2022-03-13 15:42:05 +0700

    update to 1.18.1

--- src/main/java/vn/turito/minefetch/Minefetch.java
+++ src/main/java/vn/turito/minefetch/Minefetch.java
@@ -9,17 +9,16 @@ import net.minecraft.text.*;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 
-
 public class Minefetch implements ModInitializer {
 	private int Fetch(CommandContext<ServerCommandSource> ctx) throws CommandSyntaxException {
 		ServerCommandSource sauce = ctx.getSource();
-		ServerWorld world         = sauce.getWorld();
 		ServerPlayerEntity player = sauce.getPlayer();
+		ServerWorld world         = sauce.getWorld();
 
 		String positions = player.getBlockX()+", "+player.getBlockY()+", "+player.getBlockZ();
 
 		Text[] fetches = {
-			new LiteralText(String.valueOf("██████████  "+"@"+player.getDisplayName().asString())),
+			new LiteralText(String.valueOf("██████████  "+"@")+player.getDisplayName()),
 			new LiteralText(String.valueOf("██░░██░░██  "+"Seed: "+world.getSeed())),
 			new LiteralText(String.valueOf("████░░████  "+"Time: "+world.getTime())),
 			new LiteralText(String.valueOf("███░░░░███  "+"Position: "+positions)),
